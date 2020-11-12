67af72418f1c4add2986b81577a5d3bea326355f
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2020-11-12 15:19:04 -0300

    complete scenario

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -1,11 +1,12 @@
-Given(/^the input "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^the input "([^"]*)"$/) do |input|
+  @input = input
 end
 
 When(/^the calculator is run$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  @output = `ruby calc.rb #{@input}`
+  raise('Command failed!') unless $?.success?
 end
 
-Then(/^the output should be "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^the output should be "([^"]*)"$/) do |expected_output|
+  raise('Command failed!') unless @output == expected_output
 end
